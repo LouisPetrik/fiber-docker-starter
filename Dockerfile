b84f09cd36d36db9cf8ffea5ee56da55305e09ac
Louis <33809479+LouisPetrik@users.noreply.github.com>
2020-11-25 19:59:40 +0100

    initial

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM golang:1.15.5-alpine3.12
+RUN mkdir /app
+ADD . /app
+WORKDIR /app 
+RUN go build -o main .
+CMD ["go mod init fiber-starter", "go get -u github.com/gofiber/fiber/v2", "/app/main"]
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		msg := fmt.Sprintf("Hello World!")
+		return c.SendString(msg)
+	})
+
+	log.Fatal(app.Listen(":80"))
+}
